2ae3d9b8888323f502257fe3a7732c202b5b9b8a
kogepanh <k.hosono1209@gmail.com>
2020-11-16 20:29:22 +0900

    フィボナッチ数列

---
+++ section04/q06.cpp
@@ -0,0 +1,22 @@
+// フィボナッチ数列
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int fibo(int N){
+    cout << "fibo(" << N << ") を呼び出しました" << endl;
+
+    if( N == 0 ) return 0;
+    else if( N == 1 ) return 1;
+
+    int result = fibo( N-1 ) + fibo( N-2 );
+    cout << N << " 項目 = " << result << endl;
+
+    return result;
+}
+
+int main(){
+    fibo(5);
+
+    return 0;
+}
