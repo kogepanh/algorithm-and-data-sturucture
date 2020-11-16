fc3ef30f3f15c5aafb18e2b7c9c9caf805eb8a53
kogepanh <k.hosono1209@gmail.com>
2020-11-16 20:12:23 +0900

    ユークリッド互除法による最大公約数

---
+++ section04/q04.cpp
@@ -0,0 +1,17 @@
+// ユークリッド互除法による最大公約数
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int GCD(int m, int n){
+    if(n == 0) return m;
+
+    return GCD(n, m % n);
+}
+
+int main(){
+    cout << GCD(51, 15) << endl;
+    cout << GCD(15, 51) << endl;
+
+    return 0;
+}
