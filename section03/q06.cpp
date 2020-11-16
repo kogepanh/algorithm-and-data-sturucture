d269111c09ca79d8992e6fabaf507b772397dfab
kogepanh <k.hosono1209@gmail.com>
2020-11-16 19:44:59 +0900

    部分和問題に対するビットを用いる全探索解法

---
+++ section03/q06.cpp
@@ -0,0 +1,28 @@
+// 部分和問題に対するビットを用いる全探索解法
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int N, W;
+    cin >> N >> W;
+    vector<int> a(N);
+    for(int i=0; i<N; ++i) cin >> a[i];
+
+    bool exist = false;
+    for(int bit=0; bit < (1<<N); ++bit){
+        int sum=0;
+        for(int i=0; i<N; ++i){
+            if(bit & (1 << i)){
+                sum += a[i];
+            }
+        }
+
+        if(sum == W) exist = true;
+    }
+
+    if(exist) cout << "Yes" << endl;
+    else cout << "No" << endl;
+
+    return 0;
+}
