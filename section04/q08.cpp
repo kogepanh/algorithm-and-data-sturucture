716528e684cb67ec8926bd007ef96eaecd5cd7bb
kogepanh <k.hosono1209@gmail.com>
2020-11-16 20:59:13 +0900

    フィボナッチ数列

---
+++ section04/q08.cpp
@@ -0,0 +1,27 @@
+// フィボナッチ数列
+
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<long long> memo;
+
+long long fibo(int N){
+    if(N == 0) return 0;
+    else if(N == 1) return 1;
+
+    if(memo[N] != -1) return memo[N];
+
+    return memo[N] = fibo(N-1) + fibo(N-2);
+}
+
+int main(){
+    memo.assign(50, -1);
+    
+    fibo(49);
+
+    for(int N=2; N<50; ++N){
+        cout << N << " 項目: " << memo[N] << endl;
+    }
+
+    return 0;
+}
