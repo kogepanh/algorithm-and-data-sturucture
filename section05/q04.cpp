a9dd52afa6ff25f8240fec1a49e0f6916f8bce0d
kogepanh <k.hosono1209@gmail.com>
2020-12-05 12:02:11 +0900

    Flog問題を配る遷移形式で解く

---
+++ section05/q04.cpp
@@ -0,0 +1,16 @@
+// Flog問題を「配る遷移形式」で解く
+#include <bits/stdc++.h>
+using namespace std;
+
+template<class T> void chmin(T& a, T b){
+    if(a > b){
+        a = b;
+    }
+}
+
+const long long INF = 1LL << 60;    // 十分大きい値
+
+int main(){
+
+    return 0;
+}
