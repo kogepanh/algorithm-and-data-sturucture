7f6333fde39d8641acb987eb4c44bdafa131ffe7
kogepanh <k.hosono1209@gmail.com>
2020-12-05 15:53:16 +0900

    edit

--- section05/q01.cpp
+++ section05/q01.cpp
@@ -5,24 +5,30 @@ using namespace std;
 
 const long long INF = 1LL << 60;
 
-int main(){
+int main()
+{
     int n;
     cin >> n;
     vector<int> h(n);
-    for(int i=0; i<n; i++) cin >> h[i];
+    for (int i = 0; i < n; i++)
+        cin >> h[i];
 
     vector<long long> dp(n, INF);
     dp[0] = 0;
 
-    for(int i=0; i<n; i++){
-        if(i == 1){
-            dp[i] = abs(h[i] - h[i-1]);
-        }else{
-            dp[i] = min( dp[i-1] + abs(h[i] - h[i-1]), dp[i-2] + abs(h[i] - h[i-2]));
+    for (int i = 0; i < n; i++)
+    {
+        if (i == 1)
+        {
+            dp[i] = abs(h[i] - h[i - 1]);
+        }
+        else
+        {
+            dp[i] = min(dp[i - 1] + abs(h[i] - h[i - 1]), dp[i - 2] + abs(h[i] - h[i - 2]));
         }
     }
 
-    cout << dp[n-1] << endl;
+    cout << dp[n - 1] << endl;
 
     return 0;
 }
--- section05/q03.cpp
+++ section05/q03.cpp
@@ -3,31 +3,38 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-template<class T> void chmin(T& a, T b){
-    if(a > b){
+template <class T>
+void chmin(T &a, T b)
+{
+    if (a > b)
+    {
         a = b;
     }
 }
 
 const long long INF = 1LL << 60;
 
-int main(){
+int main()
+{
     int n;
     cin >> n;
     vector<int> h(n);
-    for(int i=0; i<n; i++) cin >> h[i];
+    for (int i = 0; i < n; i++)
+        cin >> h[i];
 
     vector<long long> dp(n, INF);
     dp[0] = 0;
 
-    for(int i=1; i<n; i++){
-        chmin(dp[i], dp[i-1] + abs(h[i] - h[i-1]));
-        if(i > 1){
-            chmin(dp[i], dp[i-2] + abs(h[i] - h[i-2]));
+    for (int i = 1; i < n; i++)
+    {
+        chmin(dp[i], dp[i - 1] + abs(h[i] - h[i - 1]));
+        if (i > 1)
+        {
+            chmin(dp[i], dp[i - 2] + abs(h[i] - h[i - 2]));
         }
     }
 
-    cout << dp[n-1] << endl;
+    cout << dp[n - 1] << endl;
 
     return 0;
 }
