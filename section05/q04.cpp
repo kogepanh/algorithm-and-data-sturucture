c4ac97ec5f5351366db046559bc314b823b173a5
kogepanh <k.hosono1209@gmail.com>
2020-12-05 14:05:28 +0900

    Frog問題を配る遷移形式で解く

--- section05/q04.cpp
+++ section05/q04.cpp
@@ -1,4 +1,4 @@
-// Flog問題を「配る遷移形式」で解く
+// Frog問題を「配る遷移形式」で解く
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -11,6 +11,25 @@ template<class T> void chmin(T& a, T b){
 const long long INF = 1LL << 60;    // 十分大きい値
 
 int main(){
+    int N;
+    cin >> N;
+    vector<long long> h(N);
+    for(int i=0; i<N; i++) cin >> h[i];
+
+    vector<long long> dp(N, INF);
+
+    dp[0] = 0;
+
+    for(int i=0; i<N; i++){
+        if(i+1 < N){
+            chmin(dp[i+1], dp[i] + abs(h[i] - h[i+1]));
+        }
+        if(i+2 < N){
+            chmin(dp[i+2], dp[i] + abs(h[i] - h[i+2]));
+        }
+    }
+
+    cout << dp[N-1] << endl;
 
     return 0;
 }
